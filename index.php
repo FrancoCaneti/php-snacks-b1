55ae1a80f9c651f4a74bd89749abca177cbbe31b
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-18 16:04:46 +0200

    partite

--- index.php
+++ index.php
@@ -8,5 +8,69 @@
 </head>
 <body>
     
+<?php 
+/*Snack 1:
+Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite
+di basket di un’ipotetica tappa del calendario.Ogni array della partita avrà una squadra
+di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla 
+squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 */
+
+$matches = [
+    [
+        "squadraCasa" => "Brindisi",
+        "squadraOpstite" =>"Dinamo Sassari",
+        "puntiCasa"=>"54",
+        "puntiOspite"=>"56",
+    ],
+   [
+        "squadraCasa" =>"Cremona",
+        "squadraOpstite" =>"Reggiana",
+        "puntiCasa"=>"72",
+        "puntiOspite"=>"65",
+    ],
+     [
+        "squadraCasa" =>"Olimpia Milano",
+        "squadraOpstite" =>" Cantù",
+        "puntiCasa"=>"55",
+        "puntiOspite"=>"60",
+    ],
+    [
+        "squadraCasa" =>"Varese",
+        "squadraOpstite" =>"Trento",
+        "puntiCasa"=>"63",
+        "puntiOspite"=>"68",
+    ],
+     [
+        "squadraCasa" =>"Cantù",
+        "squadraOpstite" =>"Olimpia Milano",
+        "puntiCasa"=>"71",
+        "puntiOspite"=>"89",
+    ],
+];
+ ?>
+<h1>Partite</h1>
+       
+<?php for ($i = 0; $i < count($matches); $i++) { ?>
+    <div>Partita 
+     <?php echo $i+1 ?> : <?php echo $matches[$i]['squadraCasa'] ?> -
+     <?php echo $matches[$i]['squdraOspite'] ?> |
+     <?php echo $matches[$i]['puntiCasa'] ?> -
+     <?php echo $matches[$i]['puntiOspite'] ?>
+     </div>
+<?php } ?>
+
+<hr>
+<?php
+/*PHP Snack 2:
+Passare come parametri GET (query string) name, mail e age
+verificare (cercando i metodi che non
+conosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri
+2. che mail contenga un punto e una chiocciola
+3. che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”*/ 
+?>
+
 </body>
 </html>
